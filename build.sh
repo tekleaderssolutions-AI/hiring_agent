ed578b5cadba22c0b4259dec2a3787c3efb33a57
tekleaderssolutions-AI <tekleaders.solutions@gmail.com>
2025-12-03 07:18:51 +0530

    Add django migrate to build.sh to fix sessions

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+# Run Django migrations (essential for sessions)
+python manage.py migrate
+
 # Run database initialization (creates extensions and tables)
 python manage.py init_db
